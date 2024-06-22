ceaee1d1ca4d2e24e3f67a94c873fa7b50acbfcd
Matt Buckley <mattbuckley@google.com>
2024-06-22 12:13:13 +0530

    sm6150-common: Enable HWUI adpf
    Bug: b/261628396
    Test: manual
    Change-Id: I7352f933993770498d3403aeb4e45fd3f1afab88
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- properties.mk
+++ properties.mk
@@ -188,6 +188,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.mdpcomp.logs=0 \
     debug.sf.hw=0 \
     debug.sf.latch_unsignaled=1 \
+    debug.sf.enable_adpf_cpu_hint=true \
     persist.demo.hdmirotationlock=false \
     persist.sys.sf.color_saturation=1.0 \
     persist.sys.sf.native_mode=1 \
