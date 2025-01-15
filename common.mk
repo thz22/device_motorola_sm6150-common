5f13fdf2ee8126025de685cc2815c99329b6f07f
Thiago <diasthiago@protonmail.com>
2025-01-15 19:52:43 -0300

    sm6150-common: add {commonsys/commonsys-intf}/display to namespace_imports

--- common.mk
+++ common.mk
@@ -517,6 +517,8 @@ PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
+    vendor/qcom/opensource/commonsys/display \
+    vendor/qcom/opensource/commonsys-intf/display \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client
 
