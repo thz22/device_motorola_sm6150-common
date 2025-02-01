3a86fedc95b9bbc83bb563ffb471adb7896ca89a
thz22 <thjr5965@gmail.com>
2025-02-01 11:06:47 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -239,6 +239,9 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 # leave less information available via JDWP.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
+# Reduce SystemServer Debug
+PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
+
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
