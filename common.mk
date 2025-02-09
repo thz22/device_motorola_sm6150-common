bc770269c1c9a4181e7995cab9dac4b5b789a611
thz22 <thjr5965@gmail.com>
2025-02-09 12:06:40 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -110,7 +110,7 @@ TARGET_EXCLUDES_AUDIOFX := true
 
 # Moto Camera 2
 TARGET_MOTCAMERA2 := whitney
-
+TARGET_USES_MOTCAMERA2 := true
 $(call inherit-product, vendor/motorola/MotCamera2/motcamera2.mk)
 
 # Audio
